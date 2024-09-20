ccac74a5bffb2ed0b28d495c3f0990259e6c504d
PengXinyang <148336099@qq.com>
2024-09-20 20:28:11 +0800

    V2024.09.20

---
+++ document-service/src/main/java/com/buaa01/illumineer_backend/DocumentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.buaa01.illumineer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentServiceApplication.class, args);
+    }
+
+}
---
+++ gateway-service/src/main/java/com/buaa01/illumineer_backend/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.buaa01.illumineer_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@EnableWebFluxSecurity
+public class SecurityConfig implements WebMvcConfigurer {
+    static final String[] ORIGINS = new String[]{"GET", "POST", "PUT", "DELETE","OPTIONS"};
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // 禁用 CSRF 保护
+                .cors().and()    // 启用 CORS 支持
+                .authorizeRequests(authorize -> authorize
+                        // 请求放开接口
+                        .antMatchers(
+                                "/user/user/account/register"
+                        )
+                        .permitAll()
+                        // 允许HTTP OPTIONS请求
+                        .antMatchers(HttpMethod.OPTIONS).permitAll()
+                        // 其他地址的访问均需验证权限
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // 允许跨域发送 cookie
+        config.setAllowedOriginPatterns(Arrays.asList("http://localhost:8091","http://localhost:9090")); // 允许所有来源
+        config.setAllowedHeaders(List.of("*")); // 允许所有请求头
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // 允许所有方法
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return http.build();
+    }
+}
