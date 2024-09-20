9a0601f892375143bf8d6d66f51832b0ab7ef130
PengXinyang <148336099@qq.com>
2024-09-20 20:16:43 +0800

    V2024.09.20

--- user-service/src/main/java/com/buaa01/illumineer_backend/UserServiceApplication.java
+++ user-service/src/main/java/com/buaa01/illumineer_backend/UserServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IllumineerBackendApplication {
+public class UserServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IllumineerBackendApplication.class, args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 
 }
