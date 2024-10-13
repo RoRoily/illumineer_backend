041377f311665c1abf0778e275dd081038062161
lizhengyang2023 <lizhengyang2019@126.com>
2024-10-13 16:25:56 +0800

    commit files of AI assistant

---
+++ document-service/src/main/java/com/buaa01/illumineer_backend/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.buaa01.illumineer_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+//    @Bean
+//    public String string() {
+//        return "";
+//    }
+}
