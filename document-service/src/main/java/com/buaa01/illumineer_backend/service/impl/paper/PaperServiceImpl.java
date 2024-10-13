dbc25fa2bef78b197b0b8bc7b2e2430f1c9709cb
张旖霜 <yeestiew@gmail.com>
2024-10-13 09:34:31 +0800

    init PaperService

---
+++ document-service/src/main/java/com/buaa01/illumineer_backend/service/impl/paper/PaperServiceImpl.java
@@ -0,0 +1,4 @@
+package com.buaa01.illumineer_backend.service.impl.paper;
+
+public class PaperServiceImpl {
+}
---
+++ document-service/src/main/java/com/buaa01/illumineer_backend/service/paper/PaperService.java
@@ -0,0 +1,4 @@
+package com.buaa01.illumineer_backend.service.paper;
+
+public interface PaperService {
+}
