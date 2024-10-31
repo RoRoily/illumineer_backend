fddfeeb6ab468aa7f6e7d6048e978d6fcc55790f
张旖霜 <yeestiew@gmail.com>
2024-10-31 14:49:53 +0800

    修改searchPapers和advancedSearchPapers

--- document-service/src/main/java/com/buaa01/illumineer_backend/mapper/PaperMapper.java
+++ document-service/src/main/java/com/buaa01/illumineer_backend/mapper/PaperMapper.java
@@ -2,11 +2,19 @@ package com.buaa01.illumineer_backend.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.buaa01.illumineer_backend.entity.Paper;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
+@Mapper
 public interface PaperMapper extends BaseMapper<Paper> {
 
     // 获取指定文献ID的详细信息
     @Select("select * from paper where pid = #{pid} and status = 0")
     Paper getPaperByPid(Integer pid);
+
+    // 获取高级检索结果
+    @Select("select * from paper where #{condStr}")
+    List<Paper> getAdvancedSearchPapers(String condStr);
 }
