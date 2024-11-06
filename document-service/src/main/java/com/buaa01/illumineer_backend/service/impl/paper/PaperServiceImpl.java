89003c5c40c8d198d9ea5de8f66a0ad588791b55
张旖霜 <yeestiew@gmail.com>
2024-11-06 19:57:38 +0800

    实现advancedSearchPapers

--- document-service/src/main/java/com/buaa01/illumineer_backend/service/impl/paper/PaperServiceImpl.java
+++ document-service/src/main/java/com/buaa01/illumineer_backend/service/impl/paper/PaperServiceImpl.java
@@ -43,19 +43,19 @@ public class PaperServiceImpl implements PaperService {
         paper = paperMapper.getPaperByPid(pid);
 
         Map<String, Object> map = new HashMap<>();
-        map.put("title",paper.getTitle());
+        map.put("title", paper.getTitle());
         map.put("essAbs", paper.getEssAbs());
-        map.put("keywords",paper.getKeywords());
-        map.put("contentUrl",paper.getContentUrl());
-        map.put("auths",paper.getAuths());
-        map.put("field",paper.getField());
-        map.put("type",paper.getType());
-        map.put("theme",paper.getTheme());
-        map.put("publishDate",paper.getPublishDate());
-        map.put("derivation",paper.getDerivation());
+        map.put("keywords", paper.getKeywords());
+        map.put("contentUrl", paper.getContentUrl());
+        map.put("auths", paper.getAuths());
+        map.put("field", paper.getField());
+        map.put("type", paper.getType());
+        map.put("theme", paper.getTheme());
+        map.put("publishDate", paper.getPublishDate());
+        map.put("derivation", paper.getDerivation());
         map.put("ref_times", paper.getRef_times());
-        map.put("fav_times",paper.getFav_time());
-        map.put("refs",paper.getRefs());
+        map.put("fav_times", paper.getFav_time());
+        map.put("refs", paper.getRefs());
         customResponse.setData(map);
         return customResponse;
     }
@@ -73,19 +73,43 @@ public class PaperServiceImpl implements PaperService {
     // 高级检索
     @Override
     public CustomResponse advancedSearchPapers(List<Map<String, String>> conditions, Integer size, Integer offset, Integer sortType, Integer order) {
-        String condStr = "";
+        Set<Paper> papers1 = new HashSet<>();
+        Set<Paper> papers2 = new HashSet<>();
+
         for (Map<String, String> condition : conditions) {
-            if (condition.get("logic").equals("0")) {
-                condStr = condStr + condition.get("condition") + "LIKE %" + condition.get("keyword") + "%";
-            } else if (condition.get("logic").equals("1")) {
-                condStr = condStr + " AND " + condition.get("condition") + "LIKE %" + condition.get("keyword") + "%";
-            } else if (condition.get("logic").equals("2")) {
-                condStr = condStr + " OR " + condition.get("condition") + "LIKE %" + condition.get("keyword") + "%";
-            } else if (condition.get("logic").equals("3")) {
-                condStr = condStr + condition.get("condition") + "NOT LIKE %" + condition.get("keyword") + "%";
+            // 对该查询条件进行模糊查询
+            try {
+                Query query;
+                if (condition.get("logic").equals("3")) { // NOT
+                    query = Query.of(q -> q.bool(b -> b.mustNot(mn -> mn.match(m -> m.field(condition.get("condition")).query(condition.get("keyword"))))));
+                } else {
+                    query = Query.of(q -> q.match(m -> m.field(condition.get("condition")).query(condition.get("keyword"))));
+                }
+                SearchRequest searchRequest = new SearchRequest.Builder().index("paper").query(query).build();
+                SearchResponse<Paper> searchResponse = client.search(searchRequest, Paper.class);
+                for (Hit<Paper> hit : searchResponse.hits().hits()) {
+                    if (hit.source() != null) {
+                        if (papers1.isEmpty()) {
+                            papers1.add(hit.source());
+                        } else {
+                            papers2.add(hit.source());
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                log.error("查询ES相关文献文档时出错了：" + e);
             }
+
+            // 进行集合运算
+            if (condition.get("logic").equals("1")) { // AND
+                papers1.retainAll(papers2);
+            } else if (condition.get("logic").equals("2")) { // OR
+                papers1.addAll(papers2);
+            }
+            papers2.clear();
         }
-        List<Paper> papers = paperMapper.getAdvancedSearchPapers(condStr);
+
+        List<Paper> papers = papers1.stream().toList();
         return getSearchResult(papers, sortType, order, size, offset);
     }
 
@@ -124,6 +148,7 @@ public class PaperServiceImpl implements PaperService {
 
     /**
      * 获取筛选字段的选项
+     *
      * @param papers
      */
     Map<String, List<Map.Entry<String, Integer>>> getOptions(List<Paper> papers) {
@@ -201,6 +226,7 @@ public class PaperServiceImpl implements PaperService {
 
     /**
      * 模糊查询
+     *
      * @param keyword 搜索内容
      * @return 文献信息
      */
@@ -224,9 +250,10 @@ public class PaperServiceImpl implements PaperService {
 
     /**
      * 分页
-     * @param papers 搜索的结果
+     *
+     * @param papers  搜索的结果
      * @param pageNum 一页的条目数量
-     * @param offset 第几页
+     * @param offset  第几页
      * @return 文献信息
      */
     List<Paper> searchByPage(List<Paper> papers, Integer pageNum, Integer offset) {
@@ -259,9 +286,10 @@ public class PaperServiceImpl implements PaperService {
 
     /**
      * 排序
-     * @param papers 搜索的结果
+     *
+     * @param papers   搜索的结果
      * @param sortType 根据这个来进行排序 // 1=publishDate出版时间，2=ref_times引用次数，3=fav_time收藏次数
-     * @param order 0=降序，1=升序
+     * @param order    0=降序，1=升序
      * @return 文献信息
      */
     List<Paper> searchByOrder(List<Paper> papers, Integer sortType, Integer order) {
@@ -302,6 +330,7 @@ public class PaperServiceImpl implements PaperService {
 
     /**
      * 根据 pid 返回引用量
+     *
      * @param pid 文章 id
      * @return 引用量
      */
@@ -314,13 +343,14 @@ public class PaperServiceImpl implements PaperService {
         paper = paperMapper.getPaperByPid(pid);
 
         Map<String, Object> map = new HashMap<>();
-        map.put("ref_times",paper.getRef_times());
+        map.put("ref_times", paper.getRef_times());
         customResponse.setData(map);
         return customResponse;
     }
 
     /**
      * 根据 pid 增加引用量
+     *
      * @param pid 文章 id
      */
     public CustomResponse addRefTimes(int pid) {
@@ -337,6 +367,7 @@ public class PaperServiceImpl implements PaperService {
 
     /**
      * 根据 pid 增加收藏量
+     *
      * @param pid 文章 id
      */
     public CustomResponse addFavTimes(int pid) {
@@ -353,7 +384,8 @@ public class PaperServiceImpl implements PaperService {
 
     /**
      * 根据 pid 上传新的文章
-     * @param paper 文章
+     *
+     * @param paper   文章
      * @param content 文章内容（文件）
      */
     public CustomResponse uploadPaper(Paper paper, MultipartFile content) {
@@ -381,6 +413,7 @@ public class PaperServiceImpl implements PaperService {
 
     /**
      * 更新作者（已认证）
+     *
      * @param pid
      * @param aid
      * @return
@@ -421,6 +454,7 @@ public class PaperServiceImpl implements PaperService {
 
     /**
      * 更新作者（已认证）
+     *
      * @param pid
      * @param author
      * @return
@@ -455,6 +489,7 @@ public class PaperServiceImpl implements PaperService {
 
     /**
      * 删除文章
+     *
      * @param pid
      * @return
      */
@@ -472,6 +507,7 @@ public class PaperServiceImpl implements PaperService {
 
     /**
      * 修改文章信息
+     *
      * @param
      * @return
      */
