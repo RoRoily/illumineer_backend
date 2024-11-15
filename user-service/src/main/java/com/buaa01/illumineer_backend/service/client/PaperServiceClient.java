4c0c9077f886eef94bbafa68cce46e5f5bf684ea
ROILYY <zhengmohum@gmail.com>
2024-11-15 21:23:19 +0800

    some update

--- user-service/src/main/java/com/buaa01/illumineer_backend/service/client/PaperServiceClient.java
+++ user-service/src/main/java/com/buaa01/illumineer_backend/service/client/PaperServiceClient.java
@@ -1,11 +1,8 @@
 package com.buaa01.illumineer_backend.service.client;
 
 
-import com.bilimili.buaa13.config.FeignConfig;
-import com.bilimili.buaa13.entity.ResponseResult;
-import com.bilimili.buaa13.entity.Video;
-import com.bilimili.buaa13.entity.VideoStatus;
-import com.bilimili.buaa13.service.fallback.VideoServiceClientFallback;
+import com.buaa01.illumineer_backend.entity.CustomResponse;
+import com.buaa01.illumineer_backend.entity.Paper;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,32 +10,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 //服务名称和url
-//用于接收video微服务模块传输的对象
-//自动将调用路由到'video-service'
-@FeignClient(name = "video-service", configuration = FeignConfig.class,fallback = VideoServiceClientFallback.class)
-public interface VideoServiceClient {
+//用于接收document微服务模块传输的对象
+//自动将调用路由到' document-service'
+@FeignClient(name = "document-service", configuration = FeignConfig.class,fallback = PaperServiceClientFallback.class)
+public interface PaperServiceClient {
 
-    //从videoService中寻找提供的服务
-    @GetMapping("/video/{vid}")
-    Video getVideoById(@PathVariable("vid") Integer vid);
+    //从documentService中寻找提供的服务
+    /**
+     * 查询函数
+     * */
+    @GetMapping("/document/{pid}")
+    Paper getPaperById(@PathVariable("pid") Integer pid);
 
-    @GetMapping("/video/videoStatus/{vid}")
-    VideoStatus getVideoStatusById(@PathVariable("vid") Integer vid);
+    @GetMapping("/document/documentStatus/{pid}")
+    PaperStatus getPaperStatusById(@PathVariable("pid") Integer pid);
 
-    @PostMapping("/video/updateStatus")
-    ResponseResult updateVideoStatus(@RequestParam("vid") Integer vid,
+    @PostMapping("/document/updateStatus")
+    CustomResponse updatePaperStatus(@RequestParam("pid") Integer pid,
                                      @RequestParam("statusType") String statusType,
                                      @RequestParam("increment") Boolean increment,
                                      @RequestParam("count") Integer count);
 
 
-    @PostMapping("/video/updateGoodAndBad")
-    ResponseResult updateGoodAndBad(@RequestParam("vid") Integer vid,
-                                    @RequestParam("addGood") Boolean addGood
-    );
-    @GetMapping("/video/provider/test/{message}")
-    public String getProviderTest(@PathVariable("message") String message);
+    @GetMapping("/document/propider/test/{message}")
+    public String getPropiderTest(@PathVariable("message") String message);
 
-    @GetMapping("/video/provider/sentinel/test/{message}")
-    public String providerSentinelTest(@PathVariable("message") String message);
+    @GetMapping("/document/propider/sentinel/test/{message}")
+    public String propiderSentinelTest(@PathVariable("message") String message);
 }
