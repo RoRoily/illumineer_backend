112e777e1c7940fcd137911a86336234d7bbcfba
PengXinyang <148336099@qq.com>
2024-12-03 18:20:14 +0800

    V2024.12.3

--- document-service/src/main/java/com/buaa01/illumineer_backend/DocumentServiceApplication.java
+++ document-service/src/main/java/com/buaa01/illumineer_backend/DocumentServiceApplication.java
@@ -1,9 +1,11 @@
 package com.buaa01.illumineer_backend;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
 public class DocumentServiceApplication {
 
     public static void main(String[] args) {
