7d0b6b81e1e3662db5a88b53413de2a823c81ab5
张旖霜 <yeestiew@gmail.com>
2024-12-03 17:49:19 +0800

    document-service 的 service 部分修改 bug

--- document-service/src/main/java/com/buaa01/illumineer_backend/entity/SearchResultPaper.java
+++ document-service/src/main/java/com/buaa01/illumineer_backend/entity/SearchResultPaper.java
@@ -12,7 +12,7 @@ import java.util.Map;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SearchResultPaper {
-    private Integer pid;
+    private Long pid;
     /** 文章题目 */
     private String title;
     /** 关键词 */
@@ -20,8 +20,7 @@ public class SearchResultPaper {
     /** 文章作者 */
     private Map<String, Integer> auths;
     /** 相关领域 */
-    private List<Category> field;
-    // private List<String> category;// 统一相关领域
+    private String field;
     /** 文章类型 */
     private String type;
     /** 文章主题 */
