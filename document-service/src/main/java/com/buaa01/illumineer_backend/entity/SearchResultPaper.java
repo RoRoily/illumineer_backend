6f902b1d685f12d3b7ab2afebb9aef4d68b82382
sql <2244509212@qq.com>
2024-12-03 16:38:00 +0800

    small fix

--- document-service/src/main/java/com/buaa01/illumineer_backend/entity/SearchResultPaper.java
+++ document-service/src/main/java/com/buaa01/illumineer_backend/entity/SearchResultPaper.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
