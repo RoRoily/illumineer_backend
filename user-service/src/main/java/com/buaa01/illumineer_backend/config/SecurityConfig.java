5cf0149b067e14e323bb2dfeb9789445d4fe219b
PengXinyang <148336099@qq.com>
2024-12-04 20:08:33 +0800

    V2024.12.3

--- user-service/src/main/java/com/buaa01/illumineer_backend/config/SecurityConfig.java
+++ user-service/src/main/java/com/buaa01/illumineer_backend/config/SecurityConfig.java
@@ -1,6 +1,13 @@
 package com.buaa01.illumineer_backend.config;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.context.annotation.Bean;
@@ -12,12 +19,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import java.util.Objects;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 
+
     private final AuthenticationProvider authenticationProvider;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     public SecurityConfig(AuthenticationProvider authenticationProvider) {
         this.authenticationProvider = authenticationProvider;
@@ -39,5 +51,35 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 用户名和密码验证
+     * @return Authentication对象
+     */
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        return new AuthenticationProvider() {
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                // 从Authentication对象中获取用户名和身份凭证信息
+                String username = authentication.getName();
+                String password = authentication.getCredentials().toString();
+
+                UserDetails loginUser = userDetailsService.loadUserByUsername(username);
+                if (Objects.isNull(loginUser) || !passwordEncoder().matches(password, loginUser.getPassword())) {
+                    // 密码匹配失败抛出异常
+                    throw new BadCredentialsException("访问拒绝：用户名或密码错误！");
+                }
+
+//                log.info("访问成功：" + loginUser);
+                return new UsernamePasswordAuthenticationToken(loginUser, password, loginUser.getAuthorities());
+            }
+
+            @Override
+            public boolean supports(Class<?> authentication) {
+                return authentication.equals(UsernamePasswordAuthenticationToken.class);
+            }
+        };
     }
 }
