6b1b2553739f382013c66d7de940aeaf5b607e88
PengXinyang <148336099@qq.com>
2024-12-24 20:47:52 +0800

    V2024.12.24

--- user-service/src/main/java/com/buaa01/illumineer_backend/service/impl/user/UserServiceImpl.java
+++ user-service/src/main/java/com/buaa01/illumineer_backend/service/impl/user/UserServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
