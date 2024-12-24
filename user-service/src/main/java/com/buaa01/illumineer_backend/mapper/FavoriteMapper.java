fbbf6ef4848a818ff6805e86407289df38047afd
sql <2244509212@qq.com>
2024-12-24 19:35:46 +0800

    fix favourites

--- user-service/src/main/java/com/buaa01/illumineer_backend/mapper/FavoriteMapper.java
+++ user-service/src/main/java/com/buaa01/illumineer_backend/mapper/FavoriteMapper.java
@@ -1,13 +1,24 @@
 package com.buaa01.illumineer_backend.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
+import com.buaa01.illumineer_backend.entity.Category;
 import com.buaa01.illumineer_backend.entity.Favorite;
+import com.buaa01.illumineer_backend.entity.User;
 
 @Mapper
 public interface FavoriteMapper extends BaseMapper<Favorite> {
+
+    @Select("SELECT * FROM favorite WHERE fid = ${fid}")
+    Favorite selectById(String fid);
+
     @Delete("DELETE FROM favorite WHERE fid = ${fid}")
     void deleteById(Integer fid);
 }
