a8b488eb03e5d910155f389fef58f1aa078e5dee
ggy <1341695984@qq.com>
2024-12-25 12:08:16 +0800

    rename

--- document-service/src/main/java/com/buaa01/illumineer_backend/controller/PaperAdoptionController.java
+++ document-service/src/main/java/com/buaa01/illumineer_backend/controller/PaperAdoptionController.java
@@ -62,7 +62,7 @@ public class PaperAdoptionController {
      * 返回该用户G指数和H指数
      * @param name 姓名
      * **/
-    @GetMapping("/number/exponent")
+    @GetMapping("/user/exponent")
     public CustomResponse getUserPaperExponent(@RequestParam("name") String name) {
         try {
             Map<String, Object> result = (Map<String, Object>) paperAdoptionService.getPaperBelongedByName(name).getData();
